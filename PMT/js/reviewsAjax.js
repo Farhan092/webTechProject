464530d5fff22120678bb59a22e608a2785375b7
Farhan Amin <133957646+Farhan092@users.noreply.github.com>
2023-12-02 02:06:04 +0600

    Update reviewsAjax.js

--- PMT/js/reviewsAjax.js
+++ PMT/js/reviewsAjax.js
@@ -1,24 +1,24 @@
 
 function submitReview() {
   console.log('function called');
-  var rating = document.getElementById('rating').value;
-  var review = document.getElementById('review').value;
-  var serviceType = document.getElementById('service-type').value;
+  let rating = document.getElementById('rating').value;
+  let review = document.getElementById('review').value;
+  let serviceType = document.getElementById('service-type').value;
 
-  var xhttp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
 
-  xhttp.open('POST', '../model/reviewsModel.php', true);
+  xhttp.open('POST', '../view/reviews.php', true);
 
   xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-          document.getElementById('reviewMessage').innerHTML = this.responseText;
+          document.getElementById('form1').innerHTML = this.responseText;
       }
   }
 
   xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  var action = 'insertReview';
-    var params = 'action=' + action + '&rating=' + rating + '&review=' + review + '&service-type=' + serviceType;
 
+  let params = 'rating=' + rating + '&review=' + review + '&service-type=' + serviceType;
 
   xhttp.send(params);
 }
+
